376e2a7291c971c5b17395699d7ae145343ffd0b
Gui-dev <adrianon10@gmail.com>
2021-08-23 03:59:48 -0300

    chorus: add config pwa

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,19 @@
 import { AppProps } from 'next/app'
+import Head from 'next/head'
 
-import { GlobalStyle } from './../styles/global'
+import { GlobalStyle } from 'styles/global'
 
 function MyApp ({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Head>
+        <link rel="shortcut icon" href="/images/icon-512.png"/>
+        <link rel="apple-touch-icon" href="/images/icon-512.png"/>
+        <link rel="manifest" href="/manifest.json"/>
+        <meta name="theme-color" content="#06092B"/>
+        <title>React (NextJS) - Boilerplate</title>
+        <meta name="description" content="A simple project starter to work with Typescript, React, NextJS and Styled Components"/>
+      </Head>
       <GlobalStyle />
       <Component {...pageProps} />
     </>
